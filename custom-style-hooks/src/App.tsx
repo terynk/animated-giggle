caa9a102ecf4e46057f78da6900d990c148d4572
terynkum <terynkum@microsoft.com>
2025-02-21 13:50:50 -0800

    applied customTheme and token variable

--- custom-style-hooks/src/App.tsx
+++ custom-style-hooks/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { FANCY_CUSTOM_STYLE_HOOKS } from './theme/FancyAppCustomStyleHooksValue'
-import { FluentProvider, webLightTheme, Button } from '@fluentui/react-components'
+import { FluentProvider, Button } from '@fluentui/react-components'
 import { CustomStyleHooksProvider_unstable } from '@fluentui/react-shared-contexts'
+import { customTheme } from './theme/tokens'
 
 function App() {
 
   return (
     <>
-      <FluentProvider theme={webLightTheme}>
+      <FluentProvider theme={customTheme}>
         <CustomStyleHooksProvider_unstable value={FANCY_CUSTOM_STYLE_HOOKS}>
           <Button>Click me I'm Fancy</Button>
         </CustomStyleHooksProvider_unstable>
--- custom-style-hooks/src/theme/tokens.ts
+++ custom-style-hooks/src/theme/tokens.ts
@@ -1,3 +1,15 @@
-export const customTokens = {
-  borderRadiusMassive: '48px'
+import { Theme, tokens, webLightTheme } from "@fluentui/react-components";
+
+type CustomTheme = Theme & {
+  borderRadiusMassive: string;
+};
+
+export const customTheme: CustomTheme = {
+  ...webLightTheme,
+  borderRadiusMassive: "48px"
+};
+
+export const customTokens: Record<keyof CustomTheme, string> = {
+  ...tokens,
+  borderRadiusMassive: `var(--borderRadiusMassive)`
 };
